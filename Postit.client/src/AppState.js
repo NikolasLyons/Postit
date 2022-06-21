1cc49ca9941bafb087e2c27dd05269499a6dfc8a
Nik Lyons <nikolas.lyons23@gmail.com>
2022-06-21 16:49:39 -0600

    last Push

--- Postit.client/src/AppState.js
+++ Postit.client/src/AppState.js
@@ -5,4 +5,7 @@ export const AppState = reactive({
   user: {},
   account: {},
   albums: [],
+  activeAlbum: {},
+  albumMembers:[],
+  myAlbumMembers: []
 })
--- Postit.client/src/components/Album.vue
+++ Postit.client/src/components/Album.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="col-12 rounded shadow m-3">
+ <div class="col-12 rounded shadow m-3 album" @click="seeAlbum">
   <img :src="album.coverImg" alt="">
    {{album.name}}
   </div>
@@ -8,14 +8,20 @@
 
 <script>
 import { computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { AppState } from '../AppState'
 
 export default {
   props:{ album:{type: Object, required: false}},
   setup(props){
-    
+    const router = useRouter()
     return {
-      albums: computed(()=> AppState.albums)
+      albums: computed(()=> AppState.albums),
+
+   seeAlbum(){
+    router.push({name:'AlbumDetails',params:{id: props.album.id}})
+        
+      }
     }
   }
 }
@@ -23,5 +29,13 @@ export default {
 
 
 <style lang="scss" scoped>
+.album{
+  cursor: pointer;
+}
+
+.album:hover{
+  transform: scale(1.01);
+  transition: 1s;
+}
 
 </style>
